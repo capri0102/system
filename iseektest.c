b17a3a06f780a015f272dfbe5fe821f79c91d363
capri0102 <capri9201@gmail.com>
2016-06-30 11:59:32 +0900

    n1

---
+++ iseektest.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define NAMELENGTH 11
+#define NROOMS 10
+
+
+char namebuf[NAMELENGTH];
+int infile = -1;
+
+
+char *getoccupier(int roomno){
+	off_t offset;
+	ssize_t nread;
+
+	if(infile == -1 && (infile = open("residents", O_RDONLY)) == -1)
+		return NULL;
+	
+	offset = (roomno - 1) * NAMELENGTH;
+
+	if(lseek(infile, offset, SEEK_SET) == -1)
+		return NULL;
+	if((nread = read (infile, namebuf, NAMELENGTH)) <= 0)
+		return NULL;
+
+	namebuf[nread -1] = '\0';
+	return namebuf;
+}
+
+int main(void){
+	int j;
+	char *getoccupier (int), *p;  // prototype 
+
+	for(j=1; j <= NROOMS; j++){
+		if(p = getoccupier(j))
+			printf("Room %2d, %s\n", j, p);
+		else
+			printf("ERROR on room %d\n", j);
+	}
+
+	return 0;
+}
